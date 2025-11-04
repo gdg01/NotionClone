afe80e1ed5265422f8ac7a37df95b97141bb3784
gdg01 <dg.giusepp@gmail.com>
2025-11-04 16:41:51 +0100

    Fix(PWA) import in index,tsx

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ConvexClientProvider } from './ConvexClientProvider'; // Importa il provider
 import './index.css'
+import 'virtual:pwa-register';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
